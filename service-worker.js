026bf899349f1c2f3bc12b1204d0b533d2bd8871
Gustavo Nogueira <139269030+ducadete@users.noreply.github.com>
2025-10-30 09:18:00 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-// Ouve o clique no ícone da extensão na barra de ferramentas
-chrome.action.onClicked.addListener((tab) => {
-  // Abre o painel lateral na aba atual
-  chrome.sidePanel.open({ windowId: tab.windowId });
-});
